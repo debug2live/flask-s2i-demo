303984ce52241265aeda606532c2e2f127c3a309
thoaitrands <79261410+thoaitrands@users.noreply.github.com>
2026-03-14 09:56:23 +0700

    Initialize Flask app with hello endpoint

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_openshift():
+    return jsonify({
+        "message": "Hello OpenShift S2I!",
+        "status": "Success",
+        "version": "v1.0"
+    })
+
+if __name__ == '__main__':
+    # Lấy port từ biến môi trường, mặc định là 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
